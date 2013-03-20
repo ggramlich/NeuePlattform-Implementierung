6b3847afd2413ea9415e80e69f3e00d18b6cebe4
Gregor Gramlich <gramlich@eosc.de>
2013-03-20 21:15:13 +0100

    Ein teutsches Experiment

--- test/server-smoke-test.js
+++ test/server-smoke-test.js
@@ -6,13 +6,13 @@ var proxyquire =  require('proxyquire');
 var port = 17125;
 
 var sympaClientStub = {};
-var gruppenAppStub = proxyquire('../lib/gruppen', {'../gruppenverwaltung/swkSympaClient': sympaClientStub});
-var app = proxyquire('../app.js', {'./lib/gruppen': gruppenAppStub});
+var gruppenAnwendungStub = proxyquire('../lib/gruppen', {'../gruppenverwaltung/swkSympaClient': sympaClientStub});
+var app = proxyquire('../app.js', {'./lib/gruppen': gruppenAnwendungStub});
 
-var base_uri = "http://localhost:" + port;
-var events_uri = base_uri + '/events';
+var basis_uri = "http://localhost:" + port;
+var veranstaltungen_uri = basis_uri + '/events';
 
-describe('SWK Plattform server', function () {
+describe('SWK Plattform Server', function () {
   beforeEach(function (done) {
     app.start(port, done);
   });
@@ -22,7 +22,7 @@ describe('SWK Plattform server', function () {
   });
 
   it('responds on a GET for the home page', function (done) {
-    request({uri: base_uri}, function (req, resp) {
+    request({uri: basis_uri}, function (req, resp) {
       should.exist(resp);
       resp.statusCode.should.equal(200);
       done();
@@ -30,35 +30,35 @@ describe('SWK Plattform server', function () {
   });
 
   it('responds with HTML on a GET for the home page', function (done) {
-    request({uri: base_uri}, function (req, resp) {
+    request({uri: basis_uri}, function (req, resp) {
       resp.headers['content-type'].should.contain('text/html');
       done();
     });
   });
 
   it('shows "Softwerkskammer" on the home page', function (done) {
-    request({uri: base_uri}, function (req, resp) {
+    request({uri: basis_uri}, function (req, resp) {
       resp.body.should.contain('Softwerkskammer');
       done();
     });
   });
 
   it('shows "Upcoming events" on the event page', function (done) {
-    request({uri: events_uri}, function (req, resp) {
-      resp.body.should.contain('Upcoming events');
+    request({uri: veranstaltungen_uri}, function (req, resp) {
+      resp.body.should.contain('Zukünftige Veranstaltungen');
       done();
     });
   });
 
   it('shows "Event X" for GET /events/X', function (done) {
-    request({uri: events_uri + '/X'}, function (req, resp) {
-      resp.body.should.contain('Event X');
+    request({uri: veranstaltungen_uri + '/X'}, function (req, resp) {
+      resp.body.should.contain('Veranstaltung X');
       done();
     });
   });
 
   it('provides the style sheet', function (done) {
-    var stylesheet_uri = base_uri + '/stylesheets/style.css';
+    var stylesheet_uri = basis_uri + '/stylesheets/style.css';
     request({uri: stylesheet_uri}, function (req, resp) {
       resp.statusCode.should.equal(200);
       resp.headers['content-type'].should.contain('text/css');
@@ -68,13 +68,13 @@ describe('SWK Plattform server', function () {
   });
 
   it('shows the list of gruppen as retrieved by the SOAP call', function (done) {
-    var gruppenresult = [{id: 'groupid1', name: 'Gruppe 1'}];
+    var gruppen_liste = [{id: 'gruppenId1', name: 'Gruppe 1'}];
     sympaClientStub.getGruppen = function (callback) {
-      callback(null, gruppenresult);
+      callback(null, gruppen_liste);
     };
-    request({uri: base_uri + '/gruppen'}, function (req, resp) {
+    request({uri: basis_uri + '/gruppen'}, function (req, resp) {
       resp.statusCode.should.equal(200);
-      resp.body.should.contain('<a href="gruppen/groupid1">Gruppe 1</a>');
+      resp.body.should.contain('<a href="gruppen/gruppenId1">Gruppe 1</a>');
       done();
     });
   });
